d4ab89f62053569875c5ede964fd1d1514efbc7a
sahadat <shmanik14@yahoo.com>
2021-06-11 21:10:24 +0600

    changes

--- src/components/Header/Hero/Hero.js
+++ src/components/Header/Hero/Hero.js
@@ -1,13 +1,4 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+import React from 'react';
 
 const Hero = () => {
   return (
